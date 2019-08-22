36f32dd997e9a7f5aa27bc60c229d277bbebd9af
cstamper6 <42618384+cstamper6@users.noreply.github.com>
2019-08-21 21:27:12 -0400

    fork

---
+++ Region.java
@@ -0,0 +1,17 @@
+package miniapps;
+
+public interface Region {
+
+	int getTop();
+
+	int getBottom();
+
+	int getLeft();
+
+	int getRight();
+
+	Region intersect(Region other) throws NoIntersectionException;
+
+	Region union(Region other);
+
+}
