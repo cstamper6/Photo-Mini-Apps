a771102bbb44e4097cf8daefa0486fddc2cbd814
cstamper6 <42618384+cstamper6@users.noreply.github.com>
2019-09-07 12:13:39 -0400

    full update

--- FileChooser.java
+++ FileChooser.java
@@ -2,15 +2,17 @@ package miniapps;
 
 import java.io.File;
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class FileChooser {
-	
+
 	public static File main() {
-		
-		// accepts only images
-		// throws error if null
-		
+
 		final JFileChooser fc = new JFileChooser();
+		fc.setCurrentDirectory(new File("Pictures"));
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Image Files", "jpg", "jpeg", "tif", "tiff", "bmp",
+				"png");
+		fc.setFileFilter(filter);
 		int val = fc.showOpenDialog(null);
 		if (val == JFileChooser.APPROVE_OPTION) {
 			return fc.getSelectedFile();
