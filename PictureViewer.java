df3909b71d16242d08f3403d9e013b06dcc41a86
cstamper6 <42618384+cstamper6@users.noreply.github.com>
2019-09-07 12:28:13 -0400

    full update

--- PictureViewer.java
+++ PictureViewer.java
@@ -2,29 +2,28 @@ package miniapps;
 
 import java.awt.BorderLayout;
 import java.io.IOException;
-
-import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicInternalFrameUI;
 
 public class PictureViewer {
-	public static JInternalFrame main(String[] args) throws IOException {
-		Picture p = PictureReader.readFromFile();
-		p.setCaption("KMP in Namibia");
+	public static JInternalFrame main(Picture pic) throws IOException {
+		Picture p = pic;
 		SimplePictureViewWidget simple_widget = new SimplePictureViewWidget(p);
 
 		JInternalFrame main_frame = new JInternalFrame();
-		main_frame.setTitle("Simple Picture View");
-		//main_frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 		JPanel top_panel = new JPanel();
 		top_panel.setLayout(new BorderLayout());
 		top_panel.add(simple_widget, BorderLayout.CENTER);
 		main_frame.setContentPane(top_panel);
 
+		BasicInternalFrameUI bi = (BasicInternalFrameUI) main_frame.getUI();
+		bi.setNorthPane(null);
+
 		main_frame.pack();
 		main_frame.setVisible(true);
-		
+
 		return main_frame;
 	}
 }
