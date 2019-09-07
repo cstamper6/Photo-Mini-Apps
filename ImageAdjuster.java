325c806f4d17949f3a9f6c03eb613488dc735c7a
cstamper6 <42618384+cstamper6@users.noreply.github.com>
2019-09-07 12:16:28 -0400

    full update

--- ImageAdjuster.java
+++ ImageAdjuster.java
@@ -2,30 +2,30 @@ package miniapps;
 
 import java.awt.BorderLayout;
 import java.io.IOException;
-
-import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicInternalFrameUI;
 
 public class ImageAdjuster {
 
-	public static JInternalFrame main(String[] args) throws IOException {
+	public static JInternalFrame main(Picture pic) throws IOException {
 
-		Picture p = PictureReader.readFromFile();
-		p.setCaption("KMP in Namibia");
+		Picture p = pic;
 		AdjusterPictureViewWidget adjuster_widget = new AdjusterPictureViewWidget(p);
 
 		JInternalFrame main_frame = new JInternalFrame();
-		main_frame.setTitle("ImageAdjuster");
 
 		JPanel top_panel = new JPanel();
 		top_panel.setLayout(new BorderLayout());
 		top_panel.add(adjuster_widget, BorderLayout.CENTER);
 		main_frame.setContentPane(top_panel);
 
+		BasicInternalFrameUI bi = (BasicInternalFrameUI) main_frame.getUI();
+		bi.setNorthPane(null);
+
 		main_frame.pack();
 		main_frame.setVisible(true);
-		
+
 		return main_frame;
 	}
 }
