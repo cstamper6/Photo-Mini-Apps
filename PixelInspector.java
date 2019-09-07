1eb5cb830285f49ae8ca71ca5ef5681f04b92b7d
cstamper6 <42618384+cstamper6@users.noreply.github.com>
2019-09-07 12:29:45 -0400

    full update

--- PixelInspector.java
+++ PixelInspector.java
@@ -2,31 +2,30 @@ package miniapps;
 
 import java.awt.BorderLayout;
 import java.io.IOException;
-
-import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicInternalFrameUI;
 
 public class PixelInspector {
 
-	public static JInternalFrame main(String[] args) throws IOException {
+	public static JInternalFrame main(Picture pic) throws IOException {
 
-		Picture p = PictureReader.readFromFile();
-		p.setCaption("KMP in Namibia");
+		Picture p = pic;
 		InspectorPictureViewWidget inspector_widget = new InspectorPictureViewWidget(p);
 
 		JInternalFrame main_frame = new JInternalFrame();
-		main_frame.setTitle("PixelInspector");
-		//main_frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 		JPanel top_panel = new JPanel();
 		top_panel.setLayout(new BorderLayout());
 		top_panel.add(inspector_widget, BorderLayout.CENTER);
 		main_frame.setContentPane(top_panel);
 
+		BasicInternalFrameUI bi = (BasicInternalFrameUI) main_frame.getUI();
+		bi.setNorthPane(null);
+
 		main_frame.pack();
 		main_frame.setVisible(true);
-		
+
 		return main_frame;
 	}
 }
